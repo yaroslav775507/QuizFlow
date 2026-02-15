958b052ea3efd8de42ac05007089b0bc86ff7748
yaroslav <redyar55@gmail.com>
2026-02-15 18:37:25 +0300

    test

--- src/main/java/org/example/quizflow/QuizFlowApplication.java
+++ src/main/java/org/example/quizflow/QuizFlowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuizFlowApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(QuizFlowApplication.class, args);
     }
 
