315a8b5602ab8b8d05b6ab3c46853307a2a0cafb
yaroslav <redyar55@gmail.com>
2026-02-15 17:48:51 +0300

    Initial commit

---
+++ src/main/java/org/example/quizflow/QuizFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/quizflow/QuizFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quizflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
